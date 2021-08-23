783fd3ee6d71a7f252418b5e7aba4f9ec93e830b
Bartek <bartlomiej.proczkowski@gmail.com>
2021-08-23 13:13:20 +0200

    Template

---
+++ components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-1">
+      <p className="text-center mt-1">BookMe - 2021, All Rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
