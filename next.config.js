261d6043a04419682daa3624bc5f734516c386ad
Bartek <bartlomiej.proczkowski@gmail.com>
2021-09-29 01:33:02 +0200

    Prepare for Deployment

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  reactStrictMode: true,
+};
+
+// stripe listen --events checkout.session.completed --forward-to localhost:3000/api/webhook
